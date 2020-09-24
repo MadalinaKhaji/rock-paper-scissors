aaf0159632d02dc90fa8e76dd4b4a0897a6b2816
Maddie <madalina.khaji10@gmail.com>
2020-09-24 15:27:47 +0300

    introduced countdown and next functionality

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,127 +1,155 @@
-const Game = require("./models/game.model");
-const Round = require("./models/round.model") ;
+const Game = require('./models/game.model');
+const Round = require('./models/round.model');
 
-const { getComputerPick, getRoundWinner, validateGameSettings, determineGameWinner, createElement, showElement, showCurrentDate, createRestartButton, generatePickText, generateWinnerText, generateScoreText, removeChildren } = require("./util");
+const {
+  getComputerPick,
+  getRoundWinner,
+  determineGameWinner,
+  createElement,
+  showElement,
+  createRestartButton,
+  generatePickText,
+  generateWinnerText,
+  generateScoreText,
+  removeChildren,
+} = require('./util');
 
-require("../styles.css");
+require('../styles.css');
 
 let game = new Game();
 
 let loadGame = () => {
-    // show default game settings for username and  no of rounds
-    document.getElementById("usernameInput").value = game.playerUsername;
-    document.getElementById("noofroundsInput").value = game.roundNo;
+  document.getElementById('start-button').addEventListener('click', () => {
+    startGame();
+  });
+  document.getElementById('rock-button').addEventListener('click', function () {
+    startRound('rock');
+  });
+  document.getElementById('paper-button').addEventListener('click', function () {
+    startRound('paper');
+  });
+  document.getElementById('scissors-button').addEventListener('click', function () {
+    startRound('scissors');
+  });
 
-    document.getElementById("start-button").addEventListener('click', () => {
-        startGame();
-    });
-    document.getElementById("rock-button").addEventListener('click', function() {
-        startRound("rock");
-    });
-    document.getElementById("paper-button").addEventListener('click', function() {
-        startRound("paper");
-    });
-    document.getElementById("scissors-button").addEventListener('click', function() {
-        startRound("scissors");
-    });
-
-    showCurrentDate("footer-date");
+  document.getElementById('nextBtn').addEventListener('click', () => {
+    next();
+  });
 };
 
 let startGame = () => {
-    let usernameValue = document.getElementById("usernameInput").value;
-    let noOfRoundsValue = document.getElementById("noofroundsInput").value;
+  let noOfRoundsValue = document.getElementById('noofroundsInput').value;
+  // assign default of 3 rounds
+  if (noOfRoundsValue === 'Choose...') {
+    game.roundNo = 3;
+  } else {
+    game.roundNo = +noOfRoundsValue;
+  }
 
-    if(game.playerUsername !== usernameValue || game.roundNo !== noOfRoundsValue) {
-        let validateResult = validateGameSettings(usernameValue, noOfRoundsValue);
-        if(validateResult === true) {
-            game.playerUsername = usernameValue.toString();
-            game.roundNo = Number.parseInt(noOfRoundsValue);
-            showElement("game-settings", false);
-            showElement("gameplay", true);
-        } else {
-            let warningAlert = document.getElementById("warningAlert");
-            showElement("warningAlert", false);
-            showElement("warningAlert", true);
-            warningAlert.innerText = validateResult;
-            return 0;
-        }
-    }
-    showElement("game-settings", false);
-    showElement("gameplay", true);
+  showElement('game-settings', false);
+  showElement('gameplay', true);
 };
 
 let startRound = (userPick) => {
-    if(game.rounds.length === game.roundNo) {
-        showGameResults(determineGameWinner(game.playerWins, game.computerWins));
-        return 0;
-    }
+  if (game.rounds.length === game.roundNo) {
+    showGameResults(determineGameWinner(game.playerWins, game.computerWins));
+    return 0;
+  }
 
-    let newRound = new Round(userPick);
-    let compPick = getComputerPick();
-    let winner = getRoundWinner(newRound.userPick, compPick);
+  let newRound = new Round(userPick);
+  let compPick = getComputerPick();
+  let winner = getRoundWinner(newRound.userPick, compPick);
 
-    if(winner === "user") {
-        game.playerWins++;
-    } else if(winner === "computer") {
-        game.computerWins++;
-    }
+  if (winner === 'user') {
+    game.playerWins++;
+  } else if (winner === 'computer') {
+    game.computerWins++;
+  }
 
-    game.rounds.push(newRound);
-
-    showElement("results", true);
-    showRoundResults(newRound.userPick, compPick, winner);
+  game.rounds.push(newRound);
+  showElement('gameplay', false);
+  showElement('results', true);
+  showRoundResults(newRound.userPick, compPick, winner);
 };
 
 let showRoundResults = (userPick, compPick, roundWinner) => {
-    let results = document.getElementById("results");
-    
-    removeChildren("results");
+  let results = document.getElementById('roundResults');
 
-    let roundNoText = "Round " + game.rounds.length + " Results";
-    let winnerText = generateWinnerText(roundWinner);
-    let userText = generatePickText("user", userPick);
-    let compText = generatePickText("computer", compPick);
+  removeChildren('roundResults');
 
-    let roundNoElem = createElement("p", roundNoText);
-    let winnerElem = createElement("p", winnerText);
-    let userElem = createElement("p", userText);
-    let compElem = createElement("p", compText);
+  // let roundNoText = 'Round ' + game.rounds.length + ' Results';
+  let winnerText = generateWinnerText(roundWinner);
+  let userText = generatePickText('You', userPick);
+  let compText = generatePickText('Computer', compPick);
 
-    results.appendChild(roundNoElem);
-    results.appendChild(winnerElem);
-    results.appendChild(userElem);
-    results.appendChild(compElem);   
+  // let roundNoElem = createElement('p', roundNoText);
+  let winnerElem = createElement('h3', winnerText);
+  let userElem = createElement('p', userText);
+  let compElem = createElement('p', compText);
+
+  // let countdownDisplay = "<div class='countdownDisplay'><span id='countdownText'>3</span> SEC </div><button class='btn btn-primary' id='nextBtn' onclick='alert();'>Next</button>";
+
+  // results.appendChild(roundNoElem);
+  results.appendChild(winnerElem);
+  results.appendChild(userElem);
+  results.appendChild(compElem);
+
+  // results.innerHTML += countdownDisplay;
+
+  countdown('start');
+};
+
+let next = () => {
+  showElement('gameplay', true);
+  showElement('results', false);
 };
 
 let showGameResults = (gameWinner) => {
-    let results = document.getElementById("results");
-    
-    removeChildren("results");
+  let results = document.getElementById('results');
 
-    showElement("gameplay", false);
+  removeChildren('results');
 
-    let winnerText = generateWinnerText(gameWinner);
-    let userScoreText = generateScoreText("user", game.playerWins);
-    let compScoreText = generateScoreText("computer", game.computerWins);
+  showElement('gameplay', false);
 
-    let winnerElem = createElement("p", winnerText);
-    let userScoreElem = createElement("p", userScoreText);
-    let compScoreElem = createElement("p", compScoreText);
-    let restartButtonElem = createRestartButton();
+  let winnerText = generateWinnerText(gameWinner);
+  let userScoreText = generateScoreText('user', game.playerWins);
+  let compScoreText = generateScoreText('computer', game.computerWins);
 
-    results.appendChild(winnerElem);
-    results.appendChild(userScoreElem);
-    results.appendChild(compScoreElem);
-    results.appendChild(restartButtonElem);
+  let winnerElem = createElement('p', winnerText);
+  let userScoreElem = createElement('p', userScoreText);
+  let compScoreElem = createElement('p', compScoreText);
+  let restartButtonElem = createRestartButton();
 
-    restartButtonElem.addEventListener('click', function() {
-        game.rounds = [];
-        game.playerWins = 0;
-        game.computerWins = 0;
-        showElement("gameplay", true);
-        showElement("results", false);
-    });
+  results.appendChild(winnerElem);
+  results.appendChild(userScoreElem);
+  results.appendChild(compScoreElem);
+  results.appendChild(restartButtonElem);
+
+  restartButtonElem.addEventListener('click', function () {
+    game.rounds = [];
+    game.playerWins = 0;
+    game.computerWins = 0;
+    showElement('gameplay', true);
+    showElement('results', false);
+  });
+};
+
+let countdown = (option) => {
+  let countdownText = document.getElementById('countdownText');
+
+  let secs = 3;
+
+  let countdownFunc = () => {
+    if (countdownText.textContent === '0') {
+      clearInterval(countdownInt);
+      next();
+    } else {
+      secs--;
+      countdownText.textContent = secs + '';
+    }
+  };
+
+  let countdownInt = setInterval(countdownFunc, 3000);
 };
 
 loadGame();
